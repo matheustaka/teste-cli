b63838da8815c6bcc818071d313fbeb23450368b
matheustakasaki <matheus8antunes@gmail.com>
2021-08-06 07:19:47 -0300

    condicionante no checkout para Sellers diferentes de Cassol

---
+++ hideSellerCheckout.js
@@ -0,0 +1,20 @@
+let paymentSec = document.getElementById('payment-data');
+
+let paymentForm = paymentSec.firstElementChild;
+
+// Quando estiver no pagamento
+if ($(paymentForm).hasClass('active')) {
+    let orderSellers = vtexjs.checkout.orderForm.sellers;
+    let pixLogo = $('[data-name = Pix]');
+
+    const checkSellerCassol = (orderSellers) => {
+        return orderSellers.name == 'Cassol Centerlar'
+    }
+
+    let findSellerCassol = orderSellers.find(checkSellerCassol);
+    if (findSellerCassol != undefined) {
+        pixLogo.remove()
+    } else {
+        0;
+    }
+}
