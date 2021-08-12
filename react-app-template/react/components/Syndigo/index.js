27f7af60e5f72088dfe9c15fb040d1595fa2ab7c
John Doe <matheus@tdzain.com>
2021-08-12 11:30:31 -0300

    Syndigo content pdp

---
+++ react-app-template/react/components/Syndigo/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+function Syndigo() {
+
+    return (
+        <div id="syndi_inline" className="">
+
+        </div>
+    )
+}
+
+export default Syndigo;
