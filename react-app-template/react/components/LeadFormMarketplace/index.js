459780c11fdc9ad5173a86e45a7dfdc7585252e5
matheustaka <matheus@tdzain.com>
2021-10-18 16:14:32 -0300

    Criando lead form para a lp de marketplace cassol

---
+++ react-app-template/react/components/LeadFormMarketplace/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useCallback, render } from 'react';
+import './global.css'
+import useForm from './useForm';
+
+const LeadFormMarketplace = _ => {
+  const [{ values, loading }, handleChange, handleSubmit] = useForm(),
+    [disabled, setDisabled] = useState(""),
+    [dialog, setDialog] = useState(false);
+
+  const enviarContato = () => {
+    fetch("/api/dataentities/MP/documents", {
+      "method": "POST",
+      "headers": {
+        "Accept": "application/vnd.vtex.ds.v10+json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(values)
+    })
+      .then(response => {
+        setDisabled("disabled")
+      })
+      .catch(err => {
+        console.error(err);
+      });
+
+    console.log(values);
+  };
+
+  return <div className="marketplace-form-content" id="form">
+    <div className="marketplace-form-center">
+      <form className="form-content" className={`form-content ${disabled == "disabled" && "disabled"}`} onSubmit={handleSubmit(enviarContato)}>
+        <div className="form-content-input">
+          <label>Nome:</label>
+          <input type="text" onChange={handleChange} id="name" name="name" required="required" placeholder="Nome" />
+        </div>
+        <div className="form-content-input">
+          <label>Nome Fantasia:</label>
+          <input type="text" onChange={handleChange} id="fantasy-name" name="fantasy-name" required="required" placeholder="Nome Fantasia" />
+          <label>Razão:</label>
+          <input type="text" onChange={handleChange} id="fantasy-name" name="fantasy-name" required="required" placeholder="Razão" />
+        </div>
+
+        <div className="form-content-input left">
+          <label>CNPJ:</label>
+          <input type="text" onChange={handleChange} id="cnpj" name="cnpj" required="required" placeholder="CNPJ" />
+        </div>
+
+        <div className="form-content-input left">
+          <label>Telefone:</label>
+          <input type="text" onChange={handleChange} id="telefone" name="telefone" required="required" placeholder="Telefone" />
+        </div>
+
+        <div className="form-content-input left">
+          <label>CEP:</label>
+          <input type="text" onChange={handleChange} id="cep" name="cep" required="required" placeholder="cep" />
+        </div>
+
+        <div className="form-content-input left">
+          <label>Endereço:</label>
+          <input type="text" onChange={handleChange} id="address" name="address" required="required" placeholder="address" />
+        </div>
+        <div className="form-content-input left">
+          <label>Complemento:</label>
+          <input type="text" onChange={handleChange} id="complement" name="complement" required="required" placeholder="complement" />
+        </div>
+
+        <div className="form-content-input">
+          <label>Cidade:</label>
+          <input type="text" onChange={handleChange} id="cidade" name="cidade" required="required" placeholder="cidade" />
+        </div>
+
+
+        <div className="form-content-input">
+          <label>E-mail:</label>
+          <input type="email" onChange={handleChange} id="email" name="email" required="required" placeholder="E-mail" />
+        </div>
+
+        {dialog && <span>{dialog}</span>}
+        <button>{loading ? 'Loading...' : `${disabled == "disabled" ? "Enviado!" : "Enviar"}`}</button>
+      </form>
+    </div>
+  </div>;
+}
+
+export default LeadFormMarketplace;
